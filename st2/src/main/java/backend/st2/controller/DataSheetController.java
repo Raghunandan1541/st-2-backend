2f5502d4f30cb61e065029e12f0819248bbb2616
Raghunandan1541 <72190739+Raghunandan1541@users.noreply.github.com>
2021-03-31 17:58:23 +0530

    Backend st2

---
+++ st2/src/main/java/backend/st2/controller/DataSheetController.java
@@ -0,0 +1,5 @@
+package backend.st2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
---
+++ st2/src/test/java/backend/st2/St2ApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.st2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class St2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
